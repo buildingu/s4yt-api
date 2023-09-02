6c895348efd1f9438348d65d0a653dc36adbc6fe
Jose Martinez <jmartinez070484@gmail.com>
2023-09-02 12:02:51 -0300

    multiple updates

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -18,6 +18,7 @@ class UserSeeder extends Seeder
             'password' => \Illuminate\Support\Facades\Hash::make(config('app.super_admin.password'))
         ]);
         $super_admin->assignRole(User::SUPER_ADMIN_ROLE);
+        $super_admin->markEmailAsVerified();
 
         $admin = User::create([
             'name' => config('app.admin.name'),
@@ -25,5 +26,6 @@ class UserSeeder extends Seeder
             'password' => \Illuminate\Support\Facades\Hash::make(config('app.admin.password'))
         ]);
         $admin->assignRole(User::ADMIN_ROLE);
+        $admin->markEmailAsVerified();
     }
 }
