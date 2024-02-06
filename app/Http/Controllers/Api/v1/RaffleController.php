34e654c407b6e7f16357f6591dd47bf6d94ef259
nmachuca <nmachuca@protonmail.com>
2024-02-06 19:53:29 -0300

    feat: raffle winners endpoint

--- app/Http/Controllers/Api/v1/RaffleController.php
+++ app/Http/Controllers/Api/v1/RaffleController.php
@@ -6,7 +6,12 @@ use App\Events\RaffleUpdate;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SetRaffleCoinsRequest;
 use App\Models\Coin;
+use App\Models\Country;
 use App\Models\RaffleItem;
+use App\Models\RaffleWinner;
+use App\Models\Region;
+use App\Models\User;
+use App\Models\UserVersion;
 use App\Models\Version;
 use App\Services\PlayerService;
 use App\Services\RaffleService;
@@ -60,6 +65,49 @@ class RaffleController extends Controller
         PlayerService::assignRaffleCoins($validated['raffle'],$user);
 
         return $this->sendResponse([], "Coins assigned successfully");
+    }
+
+    public function getRaffleWinners() : JsonResponse
+    {
+        $data = [];
+        $current_version_id = Version::currentVersionId();
+        $raffle_items = RaffleItem::whereHas('versions', function($q) use ($current_version_id){
+            $q->where('version_id', $current_version_id);
+        })->where('active', true)->get();
+
+        if(sizeof($raffle_items) > 0) {
+           foreach ($raffle_items as $raffle_item) {
+               $raffle_item_version_id =  ($raffle_item->versions()->withPivot(['id'])->wherePivot('version_id', $current_version_id)->first())->pivot->id;
+               $winners = RaffleWinner::where('raffle_item_version_id', $raffle_item_version_id)->get();
+
+               $winners_data = [];
+               if( sizeof($winners)> 0) {
+                   foreach ($winners as $winner) {
+                       $user = User::find(UserVersion::find($winner->user_version_id)->user_id);
+                       if($user->userable_type == 'App\Models\Player') {
+                           $player = $user->userable;
+                           $winners_data[] = [
+                               'name' => $user->name,
+                               'country_name' => Country::find($player->country_id)->name,
+                               'country_code' => Country::find($player->country_id)->iso3,
+                               'region_name' => isset($player->region_id) ?  Region::find($player->region_id)->name : null,
+                           ];
+                       }
+
+                   }
+
+                   $data[] = [
+                       'item_src' => $raffle_item->getFirstMediaUrl('main'),
+                       'partner_logo' => $raffle_item->rafflePartner->getFirstMediaUrl('logo_default'),
+                       'name' => $raffle_item->rafflePartner->organization_name,
+                       'winners' => $winners_data
+                   ];
+               }
+
+
+           }
+        }
+        return $this->sendResponse(['raffle_items' => $data], 'list of raffle winners');
     }
 
 }
