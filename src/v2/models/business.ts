3c318d6aa6d7a3ff2bb0f876d9649035351193f6
M.S.T.O.P <mstop4@gmail.com>
2024-11-01 14:53:21 -0400

    feature: add additional fields to Business schema per design

--- src/v2/models/business.ts
+++ src/v2/models/business.ts
@@ -2,8 +2,13 @@ import mongoose from 'mongoose';
 
 const businessSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  logo: String,
-  description: String,
+  logoS4yt: { type: String, default: null },
+  logoNormal: { type: String, default: null },
+  description: { type: String, default: null },
+  meetStartTime: { type: Date, default: null },
+  meetEndTime: { type: Date, default: null },
+  attachment: { type: String, default: null },
+  videoUrls: [{ type: String, default: null }],
   questions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }]
 }, {collection : "businesses"});
 
