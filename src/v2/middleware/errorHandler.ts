67cdc21f8e91f28c05725aee5233e4cc9f4c3f80
M.S.T.O.P <mstop4@gmail.com>
2024-12-31 18:34:11 -0500

    testing error handler with 500 errors

--- src/v2/middleware/errorHandler.ts
+++ src/v2/middleware/errorHandler.ts
@@ -1,14 +1,26 @@
 import { Request, Response, NextFunction } from "express";
-
-// You can use this if you want for sending errors. The errors where sent in a array last time like: {errors: []}.
-// This is actually a util lol, so put it in a util folder.
-const errorHandler = async () => {};
+import { logger } from "../utils/logger";
 
 // This can be used as a middleware thought instead of called next for the catch error in the controllers, so it doesn't send the default node server error, but that is fine.
-// const errorHandler = async (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {};
+const errorHandler = async (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const err: any = error;
+
+  if (
+    err.statusCode === 500 ||
+    err.message?.toLowerCase().includes("unexpectedly") ||
+    !err.statusCode
+  ) {
+    logger.error(err.stack || err);
+  }
+  
+  return res.status(err.statusCode || 500).json({
+      message: err.message
+  });
+};
 
 export default errorHandler;
