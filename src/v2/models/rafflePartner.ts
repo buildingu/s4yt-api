a0ed1ed1ddb84cb1793e04cd2ebb7af94ed39481
shema <dabirishema@gmail.com>
2025-01-11 17:05:09 -0600

    database name convention change

--- src/v2/models/rafflePartner.ts
+++ src/v2/models/rafflePartner.ts
@@ -2,14 +2,14 @@ import mongoose, {Document, Schema} from 'mongoose';
 
 interface RafflePartner extends Document{
     logo: string;   //URLL of the logo/image not sure
-    organizationName: string;
-    resourceLink: string;
+    organization_name: string;
+    resource_link: string;
 }
 
 const rafflePartnerSchema = new Schema<RafflePartner>({
     logo: { type: String, required: true },
-    organizationName: { type: String, required: true },
-    resourceLink: { type: String, required: true },
+    organization_name: { type: String, required: true },
+    resource_link: { type: String, required: true },
   });
 
  const RafflePartnerModel = mongoose.model<RafflePartner>('RafflePartner', rafflePartnerSchema);
