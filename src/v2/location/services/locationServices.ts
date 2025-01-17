78926e055dc229a150169d499762d3a1b6a598fc
David Bishop <davidbish2002@hotmail.com>
2025-01-17 14:53:53 -0330

    Update locationServices.ts

--- src/v2/location/services/locationServices.ts
+++ src/v2/location/services/locationServices.ts
@@ -3,29 +3,23 @@ import City from "../../models/cities";
 import Country from "../../models/countries";
 import Regions from "../../models/region";
 
-const getCities = async (regionName: string) => {
+export const getCountries = async () => {
+  const countries = await Country.find();
+  return countries;
+};
+
+export const getRegions = async (countryName: string) => {
+  const selected = await Regions.findOne({ countryName: countryName });
+  return (
+    selected?.regions ||
+    "No regions were found from your selected country, you can skip this step."
+  );
+};
+
+export const getCities = async (regionName: string) => {
   const cities = await City.find({ region_name: regionName });
   return (
     cities ||
     "No cities were found from your selected region, you can skip this step."
   );
 };
-const getCountries = async () => {
-  const countries = await Country.find();
-  return countries;
-};
-const getRegions = async (countryName: string) => {
-  const country = await Regions.collection.findOne({
-    countryName: countryName,
-  });
-  if (!country) {
-    return "No regions were found for the selected country, you can skip this step.";
-  }
-  const regionInfo = country.regions;
-  return (
-    regionInfo ||
-    "No regions were found for the selected country, you can skip this step."
-  );
-};
-
-export { getCities, getCountries, getRegions };
