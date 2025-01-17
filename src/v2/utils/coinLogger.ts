52b930f8792bdc445216096ab31dfb8e716a1de3
M.S.T.O.P <mstop4@gmail.com>
2025-01-17 14:09:21 -0500

    simplify coin tracker

--- src/v2/utils/coinLogger.ts
+++ src/v2/utils/coinLogger.ts
@@ -4,26 +4,13 @@ import User from "../typings/User";
 
 // Adds a coin transaction to a User's coin_transactions list
 export const trackCoins = async (user: HydratedDocument<User>, count: number, source: string, save: boolean = true) => {
-  try {
-    const coinsGained: CoinTransaction = {
-      source,
-      count,
-    };
+  const coinsGained: CoinTransaction = {
+    source,
+    count,
+  };
 
-    user.coin_transactions.push(coinsGained);
-    if (save) {
-      await user.save();
-    }
-
-    return {
-      success: true,
-      message: 'ok',
-    }
-  } catch (error) {
-    const err = error as Error;
-    return {
-      success: false,
-      message: err.message,
-    }
+  user.coin_transactions.push(coinsGained);
+  if (save) {
+    await user.save();
   }
 }
