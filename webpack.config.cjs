00ed122e56fb6ae9cbf3faa266595aff5242f06a
gbudjeakp <gbudjeakp@gmail.com>
2025-01-22 02:16:40 -0600

    Add webpack config

---
+++ webpack.config.cjs
@@ -0,0 +1,27 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  mode: 'production',
+  entry: '/src/v2/server.ts', 
+  target: 'node', 
+  output: {
+    path: path.resolve(__dirname, 'build'), 
+    filename: 'server.cjs', 
+    clean: true, building
+  },
+  resolve: {
+    extensions: ['.ts', '.js'], 
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/, 
+        use: 'ts-loader', 
+        exclude: /node_modules/, 
+      },
+    ],
+  },
+  externals: [nodeExternals()], 
+  devtool: 'source-map', 
+};
