caadfe1990c10088ea4527a8b15c0410d9b05187
M.S.T.O.P <mstop4@gmail.com>
2025-02-04 13:43:11 -0500

    fix verifySuperAdmin middleware

--- src/v2/admin/routes/admRoute.ts
+++ src/v2/admin/routes/admRoute.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express';
 import * as superAdminController from '../controllers/admController';
 import verifySuperAdmin from '../middleware/verifySuperAdmin';
+import { verifyAccessToken } from '../../authentication/middleware/verifyTokens';
+import verifyCsrfToken from '../../csrf/middleware/verifyCsrfToken';
 
 const router = Router();
 
-router.get('/users', verifySuperAdmin, superAdminController.getAllUsers);
-router.post('/users/kick/:userId', verifySuperAdmin, superAdminController.kickUser);
-router.post('/users/ban/:userId', verifySuperAdmin, superAdminController.banUser);
-router.get('/business', superAdminController.getAllBusinesses); // TODO: needs verifySuperAdmin
-router.post('/business', superAdminController.createBusiness); // TODO: needs verifySuperAdmin
-router.post('/chest', superAdminController.createChests); // For testing purposes
+router.get('/users', verifyCsrfToken, verifyAccessToken, verifySuperAdmin, superAdminController.getAllUsers);
+router.post('/users/kick/:userId', verifyCsrfToken, verifyAccessToken, verifySuperAdmin, superAdminController.kickUser);
+router.post('/users/ban/:userId', verifyCsrfToken, verifyAccessToken, verifySuperAdmin, superAdminController.banUser);
+router.get('/business', verifyCsrfToken, verifyAccessToken, verifySuperAdmin, superAdminController.getAllBusinesses);
+router.post('/business', verifyCsrfToken, verifyAccessToken, verifySuperAdmin, superAdminController.createBusiness);
+router.post('/chest', verifyCsrfToken, verifyAccessToken, verifySuperAdmin, superAdminController.createChests); // For testing purposes
 
 export default router;
