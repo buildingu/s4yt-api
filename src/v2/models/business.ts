df644e238afdf8aa27601b70ecbb1e932d66963c
M.S.T.O.P <mstop4@gmail.com>
2025-02-12 16:01:26 -0500

    add business_user_id and deleted fields to Business schema

--- src/v2/models/business.ts
+++ src/v2/models/business.ts
@@ -1,23 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema, Types } from 'mongoose';
 
-const businessSchema = new mongoose.Schema({
+const businessSchema = new Schema({
   name: { type: String, required: true },
   logo_s4yt: { type: String, default: null },
   logo_normal: { type: String, default: null },
   description: { type: String, default: null },
-  meet_members_confirmed: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  meet_members_interested: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  business_user_id: { type: Types.ObjectId, ref: 'User', required: true },
+  meet_members_confirmed: [{ type: Types.ObjectId, ref: 'User' }],
+  meet_members_interested: [{ type: Types.ObjectId, ref: 'User' }],
   attachment: { type: String, default: null },
   video_urls: [{ type: String, default: null }],
-  questions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }],
+  questions: [{ type: Types.ObjectId, ref: 'Question' }],
   winners: [
     {
-      winnerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      winnerId: { type: Types.ObjectId, ref: 'User' },
       award: { type: Number, default: 0, min: 0 }
     }
   ],
   award: { type: Number, default: 0, min: 0 },
-  awardedTotal: { type: Number, default: 0, min: 0 } 
+  awardedTotal: { type: Number, default: 0, min: 0 },
+  deleted: { type: Boolean, default: false }
 }, {collection : "businesses"});
 
 const Business = mongoose.model('Business', businessSchema);
