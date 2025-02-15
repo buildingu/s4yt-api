71d9cc5253496637ab7dc670f7aca28fda477532
shema <dabirishema@gmail.com>
2025-02-15 12:43:29 -0600

    auth feeatures

--- src/v2/admin/dtos/AdminDto.ts
+++ src/v2/admin/dtos/AdminDto.ts
@@ -8,3 +8,13 @@ export interface CreateBusinessRequestDto extends Request {
 export interface CreateChestRequestDto extends Request {
   body: any;
 }
+
+export interface LoginResponse {
+  token: string;
+  userData: {
+    _id: string;
+    email: string;
+    role: string;
+    businessId?: string | null;
+  };
+}
