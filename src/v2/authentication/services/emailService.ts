dd953f602ae7d05bcbd9f2592b433c751e967ec9
M.S.T.O.P <mstop4@gmail.com>
2025-02-15 15:51:01 -0500

    replace sendgrid with nodemailer

--- src/v2/authentication/services/emailService.ts
+++ src/v2/authentication/services/emailService.ts
@@ -1,20 +1,30 @@
-import sgMail from '@sendgrid/mail';
+import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 dotenv.config();
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST, 
+  port: parseInt(process.env.EMAIL_PORT as string),
+  auth: {
+    user: process.env.FROM_EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
 export const sendVerificationEmail = async (to: string, verificationToken: string) => {
   const verificationUrl = `${process.env.FRONTEND_URL}/register/verify-email/verify?token=${verificationToken}`;
-  const msg = {
-    to,
+
+  const mailOptions = {
     from: process.env.FROM_EMAIL as string,
+    to,
     subject: 'Dollars for Your Thoughts ($4YT) - Verify Your Email Address',
     html: `<p>To verify your email address for your Dollars for Your Thoughts ($4YT) account, please click the link below:</p><a href="${verificationUrl}">Verify Email</a>`,
+    attachments: [], 
   };
 
   try {
-    await sgMail.send(msg);
+    const response = await transporter.sendMail(mailOptions);
+    console.log(response);
     console.log(`Verification email sent to: ${to}`);
   } catch (error) {
     console.error(`Error sending verification email to ${to}: ${error}`);
@@ -24,15 +34,18 @@ export const sendVerificationEmail = async (to: string, verificationToken: strin
 
 export const sendResetPasswordEmail = async (to: string, resetToken: string) => {
   const resetUrl = `${process.env.FRONTEND_URL}/api/v2/auth/password?token=${resetToken}`;
-  const msg = {
-    to,
+
+  const mailOptions = {
     from: process.env.FROM_EMAIL as string,
+    to,
     subject: 'Dollars for Your Thoughts ($4YT) - Reset Your Password',
     html: `<p>To reset your password of your Dollars for Your Thoughts ($4YT) account, please click the link below:</p><a href="${resetUrl}">Reset Password</a>`,
+    attachments: [], 
   };
 
   try {
-    await sgMail.send(msg);
+    const response = await transporter.sendMail(mailOptions);
+    console.log(response);
     console.log(`Reset password email sent to: ${to}`);
   } catch (error) {
     console.error(`Error sending reset password email to ${to}: ${error}`);
