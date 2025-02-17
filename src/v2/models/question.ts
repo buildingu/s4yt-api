ee211a102cec38686647872a957ea7023944b3b2
M.S.T.O.P <mstop4@gmail.com>
2025-02-17 12:16:23 -0500

    remove prize allocation from Question schema

--- src/v2/models/question.ts
+++ src/v2/models/question.ts
@@ -3,11 +3,6 @@ import mongoose from 'mongoose';
 const questionSchema = new mongoose.Schema({
   text: { type: String, required: true },
   description: String,
-  prize_allocation: [{
-    place: Number,
-    amount: Number,
-    winner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null}
-  }],
   business: { type: mongoose.Schema.Types.ObjectId, ref: 'Business' }
 });
 
