e9394f700895af10dbaf15db700b67fcee641906
M.S.T.O.P <mstop4@gmail.com>
2025-02-28 16:52:24 -0500

    add seeder for raffle items

--- src/v2/seeders/seeder.ts
+++ src/v2/seeders/seeder.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url'; 
 import dotenv from 'dotenv';
 import { RafflePartnerModel } from '../models/rafflePartner.js';
+import RaffleItemModel from '../models/raffleItem.js';
 import connectDB from '../db/db.js';
 
 dotenv.config();
@@ -18,14 +19,19 @@ const seedDatabase = async () => {
 
     // Load seed data
     const rafflePartnerFilePath = path.join(__dirname, 'rafflePartners.json');
+    const raffleItemFilePath = path.join(__dirname, 'raffleItems.json');
+
     const rafflePartnerData = JSON.parse(fs.readFileSync(rafflePartnerFilePath, 'utf-8'));
+    const raffleItemData = JSON.parse(fs.readFileSync(raffleItemFilePath, 'utf-8'));
 
     // Clear existing data
     await RafflePartnerModel.deleteMany({});
-    console.log('Cleared existing RafflePartner data.');
+    await RaffleItemModel.deleteMany({});
+    console.log('Cleared existing data.');
 
     // Insert seed data
     await RafflePartnerModel.insertMany(rafflePartnerData);
+    await RaffleItemModel.insertMany(raffleItemData);
     console.log('User seed data inserted successfully.');
   } catch (err) {
     console.error('Error seeding database:', err);
