bbdc9121623457e55a59ad887e4c0843810de04a
M.S.T.O.P <mstop4@gmail.com>
2025-05-24 10:38:01 -0400

    reopen pre-game for recording session

--- src/v2/configs/timestamps.ts
+++ src/v2/configs/timestamps.ts
@@ -1,14 +1,14 @@
 import { GameTimestamps } from '../typings/Timestamps';
 
 const gameTimestamps = {
-  pre_game: '2025-04-18T01:00:00-04:00',
-  game_start: '2025-04-19T12:00:00-04:00',
-  review_start: '2025-04-22T12:00:00-04:00',
-  review_end: '2025-04-26T14:00:00-04:00',
-  game_end: '2025-05-03T14:00:00-04:00',
+  pre_game: '2025-05-26T01:00:00-04:00',
+  game_start: '2025-06-24T12:00:00-04:00',
+  review_start: '2025-06-25T12:00:00-04:00',
+  review_end: '2025-06-26T14:00:00-04:00',
+  game_end: '2025-06-27T14:00:00-04:00',
 }
 
-export const raffleDrawingTimestamp = '2025-04-24T14:00:00-04:00';
+export const raffleDrawingTimestamp = '2025-07-24T14:00:00-04:00';
 
 // Convert game timestamps into ISO strings
 export const isoGameTimestamps: GameTimestamps = Object.entries(gameTimestamps).reduce(
