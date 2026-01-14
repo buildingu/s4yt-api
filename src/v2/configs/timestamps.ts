a3d56d562d919d9ce058948417e0996440ced8c6
M.S.T.O.P. <mstop4@gmail.com>
2026-01-14 14:12:37 -0500

    update timestamps

--- src/v2/configs/timestamps.ts
+++ src/v2/configs/timestamps.ts
@@ -1,11 +1,11 @@
 import { GameTimestamps } from '../typings/Timestamps';
 
 const gameTimestamps = {
-  pre_game: '2025-04-18T01:00:00-04:00',
-  game_start: '2025-04-19T12:00:00-04:00',
-  review_start: '2025-04-22T12:00:00-04:00',
-  review_end: '2025-04-26T14:00:00-04:00',
-  game_end: '2025-05-03T14:00:00-04:00',
+  pre_game: '2026-04-03T00:00:00-04:00',
+  game_start: '2026-04-04T12:00:00-04:00',
+  review_start: '2026-04-22T12:00:00-04:00',
+  review_end: '2026-04-26T14:00:00-04:00',
+  game_end: '2026-05-03T14:00:00-04:00',
 }
 
 export const raffleDrawingTimestamp = '2025-04-24T14:00:00-04:00';
